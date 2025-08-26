a0a0735750027991906fbd2a2b5916884da818c0
tushan-honghong <tsm623227952@outlok.com>
2025-08-26 18:34:28 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
 /// <reference types="vitest/config" />
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
-import autpImport from 'unplugin-auto-import/vite';
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import autpImport from "unplugin-auto-import/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), autpImport({
-    imports: ['vue', 'vue-router'],
-    dirs: ['./src'],
-    dts: './src/auto-imports.d.ts'
-  })],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    plugins: [
+        vue(),
+        vueDevTools(),
+        autpImport({
+            imports: ["vue", "vue-router"],
+            dirs: ["./src"],
+            dts: "./src/auto-imports.d.ts",
+        }),
+        tailwindcss(),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
 });
