9dda959f021f5f0a70190d58a7441ac8742ba9b0
tushan-honghong <tsm623227952@outlok.com>
2025-08-27 09:36:32 +0800

    update

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,13 +1,7 @@
 <script setup></script>
 
 <template>
-    <div>
-        <span>发现您的下一个冒险</span>
-        <span>计划、探索并创造难忘的回忆</span>
-        <div>
-            <wl-input placeholder="您想去哪里？"></wl-input>
-        </div>
-    </div>
+    <div class="w-[20vw] h-[auto]"></div>
 </template>
 
 <style scoped></style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,13 @@
 <script setup></script>
 
-<template></template>
+<template>
+    <div class="w-[80vw] h-[90vh] bg-emerald-500">
+        <span>发现您的下一个冒险</span>
+        <span>计划、探索并创造难忘的回忆</span>
+        <div>
+            <wl-input placeholder="您想去哪里？"></wl-input>
+        </div>
+    </div>
+</template>
 
 <style scoped></style>
