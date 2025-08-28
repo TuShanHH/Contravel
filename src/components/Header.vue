61dd5a1018d472b613a9ae8c258c6c2abbac4be5
tushan-honghong <tsm623227952@outlok.com>
2025-08-28 15:11:04 +0800

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,11 +3,7 @@ let cityName = ref("");
 </script>
 
 <template>
-    <wl-input
-        v-model="cityName"
-        placeholder="请输入市名称"
-        @input="handleCliack()"
-    ></wl-input>
+
 </template>
 
 <style scoped></style>
