d8c8ef613fcfb463a852e8ed5c1eb3672a3b3a30
tushan-honghong <tsm623227952@outlok.com>
2025-08-29 19:00:10 +0800

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,15 @@
 <script setup>
-let cityName = ref("");
+
 </script>
 
 <template>
-    
+    <div class="w-[100vw] h-[20vh] flex">
+        <span>Airbnb</span>
+        <wl-button>房源</wl-button>
+        <wl-button>体验</wl-button>
+        <wl-button>服务</wl-button>
+        <wl-button>成为房东</wl-button>
+    </div>
 </template>
 
 <style scoped></style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,8 @@
 <script setup></script>
 
 <template>
-    <div class="w-[80vw] h-[90vh] bg-emerald-500">
-        <span>发现您的下一个冒险</span>
-        <span>计划、探索并创造难忘的回忆</span>
-        <div>
-            <wl-input placeholder="您想去哪里？"></wl-input>
-        </div>
+    <div class="w-[80vw] h-[90vh] ">
+
     </div>
 </template>
 
