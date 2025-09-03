1f31bda7f3a120356d66f775c8be95508fe3df11
tushan-honghong <tsm623227952@outlok.com>
2025-09-03 11:21:54 +0800

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,25 @@ import axios from "../axios/axiosInstance.js";
 onMounted(async () => {
     const res = await axios.get("hello/");
     console.log(res.data.message);
+
+    // const res1 = await axios.post("register/", {
+    //     username: "testuser1",
+    //     email: "test@example.com",
+    //     password: "123456",
+    // });
+
+    const res2 = await axios
+        .post("login/", {
+            username: "jane",
+            password: "wmdelel2020",
+        })
+        .then((response) => {
+            const access_token = response.data.access;
+            axios.defaults.headers.common[
+                "Authorization"
+            ] = `Bearer ${access_token}`;
+            const res3 = axios.get("protected/");
+        });
 });
 </script>
 
