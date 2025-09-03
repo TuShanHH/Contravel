ebc1f64ce5b65e86cc2b194785ab54602c009bb8
tushan-honghong <tsm623227952@outlok.com>
2025-09-03 10:30:09 +0800

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,14 @@
 <script setup>
+import axios from "../axios/axiosInstance.js";
 
+onMounted(async () => {
+    const res = await axios.get("hello/");
+    console.log(res.data.message);
+});
 </script>
 
 <template>
-    <div class="w-[100vw] h-[20vh] flex">
+    <div class="w-[100vw] h-[20vh] flex bg-amber-300">
         <span>Airbnb</span>
         <wl-button>房源</wl-button>
         <wl-button>体验</wl-button>
