fe9abe0b487964103db2c62563742bdb9e3cc352
tushan-honghong <tsm623227952@outlok.com>
2025-09-04 17:50:12 +0800

    update

--- src/views/home.vue
+++ src/views/home.vue
@@ -6,20 +6,25 @@ const headerHeight = ref("16vh");
 
 const handleScroll = () => {
     const scrollY = window.scrollY;
-    const minHeight = 10;
-    const maxHeight = 16;
-    const limit = 100;
-
-    let newHeight = maxHeight - (scrollY / limit) * (maxHeight - minHeight);
-    if (newHeight < minHeight) newHeight = minHeight;
-    if (newHeight > maxHeight) newHeight = maxHeight;
-
+    let newHeight = calculateActualHeight(scrollY);
     headerHeight.value = `${newHeight}vh`;
+};
+
+/**
+ *根据滚动距离计算出实际的高度值
+ *
+ * @param {number} distanceY 滚动的距离
+ * @returns {number} 实际的高度值
+ */
+const calculateActualHeight = (distanceY) => {
+    let ActualHeight = 16 - (distanceY / 100) * 8;
+    return ActualHeight < 8 ? 8 : ActualHeight;
 };
 
 onMounted(() => {
     window.addEventListener("scroll", handleScroll);
 });
+
 onUnmounted(() => {
     window.removeEventListener("scroll", handleScroll);
 });
@@ -28,7 +33,7 @@ onUnmounted(() => {
 <template>
     <div class="w-full h-screen">
         <header
-            class="w-full h-[16vh] fixed top-0 left-0 transition-all duration-300 ease-in-out"
+            class="w-full fixed top-0 left-0 transition-all duration-300 ease-in-out"
             :style="{ height: headerHeight }"
         >
             <Header />
