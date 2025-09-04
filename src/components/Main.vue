112cb5ad96126e24af9099e935d08bd1ef69c126
tushan-honghong <tsm623227952@outlok.com>
2025-09-04 18:28:02 +0800

    update

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,15 +4,17 @@ const city = "伦敦";
 
 <template>
     <div
-        class="w-72/100 mx-auto h-auto flex justify-center items-center"
-        v-for="value in 20"
-        :key="value"
+        class="w-72/100 mx-auto h-auto flex flex-col justify-center items-center"
     >
-        <div class="w-full h-[20vh]">
+        <div v-for="outer in 20" :key="outer" class="w-full h-[20vh]">
             <span class="w-full bg-amber-500">{{ city }}热门房源 ></span>
-            <div class="w-full h-[90%] bg-amber-950"></div>
+            <div class="flex justify-between w-full h-8">
+                <div
+                    class="h-full w-full"
+                    v-for="inner in 7"
+                    :key="`${outer}-${inner}`"
+                ></div>
+            </div>
         </div>
     </div>
 </template>
-
-<style scoped></style>
