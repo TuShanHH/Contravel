02bf6b0fa2ff99ab249001754ee44cc752deaec3
tushan-honghong <tsm623227952@outlok.com>
2025-09-30 18:58:22 +0800

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,11 +10,23 @@ const toBeLandlord = () => {
 
 <template>
     <div
-        class="border-b-1 border-b-gray-200 px-[14%] w-full h-full flex justify-between items-center bg-[linear-gradient(180deg,#ffffff_39.9%,#f8f8f8_100%)]"
+        class="border-b-1 border-b-gray-200 px-[14%] w-full h-full flex flex-row justify-between items-center bg-[linear-gradient(180deg,#ffffff_39.9%,#f8f8f8_100%)]"
     >
-        <div class="text-[#dfceea] ">Airbnb</div>
-        <div class="flex flex-col items-center justify-items-center">
+        <div class="text-[#dfceea] h-full">
             <div
+                class="flex flex-row items-center justify-between"
+                :style="{
+                    height: props.headerHeight === '8vh' ? '100%' : '50%',
+                }"
+            >
+                <img src="@/assets/icon.png" alt="" width="32" height="32" />
+                <span>Airbnb</span>
+            </div>
+        </div>
+
+        <div class="h-full flex flex-col items-center justify-between">
+            <div
+                class="h-full flex flex-row items-center justify-between"
                 :style="{
                     display: props.headerHeight === '8vh' ? 'none' : 'flex',
                 }"
@@ -23,8 +35,8 @@ const toBeLandlord = () => {
                 <wl-button>体验</wl-button>
                 <wl-button>服务</wl-button>
             </div>
-            <div>
-                <wl-button >地点</wl-button>
+            <div class="h-full flex flex-row items-center justify-between">
+                <wl-button>地点</wl-button>
                 <wl-button>入住</wl-button>
 
                 <wl-button>退房</wl-button>
@@ -32,7 +44,17 @@ const toBeLandlord = () => {
                 <wl-button>人员</wl-button>
             </div>
         </div>
-        <wl-button @click="toBeLandlord">成为房东</wl-button>
+
+        <div class="h-full">
+            <div
+                class="flex flex-row items-center justify-between"
+                :style="{
+                    height: props.headerHeight === '8vh' ? '100%' : '50%',
+                }"
+            >
+                <wl-button @click="toBeLandlord">成为房东</wl-button>
+            </div>
+        </div>
     </div>
 
     <wl-dailog v-model="open" @confirm="handleConfirm">
